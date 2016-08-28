f4923a85514a59cbf251c23ee374075dc234ff3a
fxghqc <konxiao@gmail.com>
2016-08-28 17:01:01 +0800

    add component BooksFilter

---
+++ src/components/BooksFilter/BooksFilter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import SearchBar from 'components/SearchBar'
+import classes from './BooksFilter.scss'
+
+export const BooksFilter = (props) => (
+  <div className={classes['BooksFilter']}>
+    <SearchBar {...{bookFilter: props.bookFilter, updateFilterText: props.updateFilterText}} />
+  </div>
+)
+
+BooksFilter.propTypes = {
+  bookFilter: React.PropTypes.object,
+  updateFilterText: React.PropTypes.string
+}
+
+export default BooksFilter
--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import BookList from 'components/BookList'
+import BooksFilter from 'components/BooksFilter'
 import { Link } from 'react-router'
 import SearchBar from 'components/SearchBar'
 import classes from './HomeView.scss'
@@ -68,12 +69,13 @@ export class HomeView extends React.Component {
 
     return (
       <div>
-        <button className={classes.button} onClick={showDeleteButton}>删除(显/隐)</button>
         <Link to='/newBook'><button className={classes.button}>新建</button></Link>
         <Link to='/login'><button className={classes.button}>登录</button></Link>
+        <BooksFilter {...{bookFilter, updateFilterText}} />
         <SearchBar className={classes.search} {...{updateFilterText, bookFilter}} />
         <button className={classes.button} onClick={filterOwned} >我自己的书</button>
         <button className={classes.button} onClick={filterBorrowed} >我借阅的书</button>
+        <button className={classes.button} onClick={showDeleteButton}>删除(显/隐)</button>
         <br />
         <div>
             {
