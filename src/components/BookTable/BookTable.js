eee680449378e8b77298c93dc07e71e74bb01dd4
fxghqc <konxiao@gmail.com>
2017-01-16 20:20:19 +0800

    add BookTable component

---
+++ src/components/BookTable/BookTable.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link } from 'react-router'
+import { Table } from 'antd'
+
+const columns = [{
+  title: '书名',
+  dataIndex: 'name',
+  render: (text, record) => <Link to={`books/${record.id}`}>{text}</Link>
+}, {
+  title: '数量',
+  dataIndex: 'quantity'
+}, {
+  title: '剩余',
+  render: (text, record) => <span>
+    {record.quantity - (record.borrowers ? record.borrowers.length : 0)}
+  </span>
+}]
+
+export class BookTable extends React.Component {
+  render () {
+    const { data } = this.props
+
+    const pagination = {
+      total: data.length,
+      showSizeChanger: true,
+      onShowSizeChange: (current, pageSize) => {
+        console.log('Current: ', current, ' PageSize: ', pageSize)
+      },
+      onChange: (current) => {
+        console.log('Current: ', current)
+      }
+    }
+
+    return (
+      <Table columns={columns} dataSource={data} pagination={pagination} />
+    )
+  }
+}
+
+BookTable.propTypes = {
+  data: React.PropTypes.object
+}
+
+export default BookTable
---
+++ src/components/BookTable/index.js
@@ -0,0 +1,3 @@
+import BookTable from './BookTable'
+
+export default BookTable
